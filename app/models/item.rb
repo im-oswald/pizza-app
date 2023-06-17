c1f57e3644ce67bad81d6710d8e1ee58a2250ea5
im-oswald <im-oswald@github.com>
2023-06-17 17:22:13 +0500

    Simplify item to ingredient many to many relation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
-
   enum size: {
     small: 'small',
     medium: 'medium',
     large: 'large'
   }
 
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :size, presence: true, inclusion: { in: sizes.keys }
+
   belongs_to :order
   has_many :item_ingredients, dependent: :destroy
-  has_many :add_ingredients, through: :item_ingredients, source: :ingredient, source_type: 'AddIngredient'
-  has_many :remove_ingredients, through: :item_ingredients, source: :ingredient, source_type: 'RemoveIngredient'
+  has_many :add_ingredients, -> { ItemIngredient.add_ingredients }, through: :item_ingredients, source: :ingredient
+  has_many :remove_ingredients, -> { ItemIngredient.remove_ingredients }, through: :item_ingredients, source: :ingredient
 end
--- app/models/item_ingredient.rb
+++ app/models/item_ingredient.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
-class RemoveItemIngredient < ApplicationRecord
+class ItemIngredient < ApplicationRecord
+  enum ingredient_type: { add_ingredient: 'AddIngredient', remove_ingredient: 'RemoveIngredient' }
+
+  validates :ingredient_type, presence: true, inclusion: { in: ingredient_types.keys }
+
   belongs_to :item
-  belongs_to :ingredient, polymorphic: true
+  belongs_to :ingredient
+
+  scope :add_ingredients, -> { where(ingredient_type: ingredient_types[:add_ingredient]) }
+  scope :remove_ingredients, -> { where(ingredient_type: ingredient_types[:remove_ingredient]) }
 end
