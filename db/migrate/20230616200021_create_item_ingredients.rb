1d7be39bbe5ed68a003df1faf743e661a11ca000
im-oswald <im-oswald@github.com>
2023-06-17 17:22:13 +0500

    Simplify item to ingredient many to many relation

--- db/migrate/20230616200021_create_item_ingredients.rb
+++ db/migrate/20230616200021_create_item_ingredients.rb
@@ -2,11 +2,12 @@ class CreateItemIngredients < ActiveRecord::Migration[7.0]
   def change
     create_table :item_ingredients do |t|
       t.references :item, null: false, foreign_key: true
-      t.references :ingredient, null: false, polymorphic: true
+      t.references :ingredient, null: false, foreign_key: true
+      t.string :ingredient_type, null: false
 
       t.timestamps
     end
 
-    add_index :item_ingredients, [:ingredient_type, :ingredient_id]
+    add_index :item_ingredients, [:ingredient_id, :item_id], unique: true
   end
 end
