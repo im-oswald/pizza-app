78694bb2be0a8dad3103ed9df54cac42dd551bf6
im-oswald <im-oswald@github.com>
2023-06-17 21:03:29 +0500

    Add orders view to show order details

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@
 module ApplicationHelper
   def format_timestamp(timestamp)
     # Convert the timestamp to a Time object
-    time = Time.at(timestamp)
+    time = Time.at.utc(timestamp)
 
     # Format the time as a human-readable string
     time.strftime('%B %d, %Y %H:%M')
