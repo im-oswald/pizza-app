abcae97b67d879c03115d7e6ce424a676ddfa57a
im-oswald <im-oswald@github.com>
2023-06-17 17:24:34 +0500

    Rectify a few models

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,8 @@ class Order < ApplicationRecord
     open: 'open'
   }
 
+  validates :status, presence: true, inclusion: { in: statuses.keys }
+
   has_many :items, dependent: :destroy
   has_and_belongs_to_many :promotion_codes, join_table: :orders_promotion_codes
   belongs_to :discount_code, optional: true
