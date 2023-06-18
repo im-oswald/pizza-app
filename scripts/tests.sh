0aed7a2b1b6402323bc4c65bc3c51e9b6ac2bcb3
im-oswald <im-oswald@github.com>
2023-06-18 21:27:44 +0500

    Add github workflow for rspec and docker script

---
+++ scripts/tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+docker-compose -p pizza-app run -e RAILS_ENV=test --rm web bundle exec rake db:migrate
+docker-compose -p pizza-app run -e RAILS_ENV=test --rm web bundle exec rspec "$@"
