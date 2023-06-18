f093d4a73c5f503b5a25b3dccf006717dea68d44
im-oswald <im-oswald@github.com>
2023-06-19 02:05:00 +0500

    Remove default uuid value from orders table

--- db/migrate/20230616191856_create_orders.rb
+++ db/migrate/20230616191856_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     enable_extension 'uuid-ossp'
     create_table :orders do |t|
-      t.uuid :uuid, null: false, default: 'gen_random_uuid()'
+      t.uuid :uuid, null: false
       t.string :status, default: 'open'
       t.references :discount_code, foreign_key: true
 
