5d5b48376f03d6dd831db4a3e62a62399dd29887
im-oswald <im-oswald@github.com>
2023-06-19 02:07:34 +0500

    Use v4 for uuid in orders table

--- db/migrate/20230616191856_create_orders.rb
+++ db/migrate/20230616191856_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     enable_extension 'uuid-ossp'
     create_table :orders do |t|
-      t.uuid :uuid, null: false
+      t.uuid :uuid, null: false, default: 'uuid_generate_v4()'
       t.string :status, default: 'open'
       t.references :discount_code, foreign_key: true
 
