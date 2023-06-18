ecc069d8efa6f59c14f3f3d05992fb81f5773344
im-oswald <im-oswald@github.com>
2023-06-19 02:21:07 +0500

    Add uuid ossp in orders migration

--- db/migrate/20230616191856_create_orders.rb
+++ db/migrate/20230616191856_create_orders.rb
@@ -1,5 +1,6 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
+    enable_extension 'uuid-ossp'
     create_table :orders do |t|
       t.uuid :uuid, null: false, default: 'uuid_generate_v4()'
       t.string :status, default: 'open'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_06_16_215025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
   create_table "discount_codes", force: :cascade do |t|
     t.string "code", null: false
